98e9a2fb928035592c16a460ab3f89d7fd62651f
Boyd G <tech2me.bg@gmail.com>
2021-05-16 14:50:01 -0700

    Update demo-limiter.sh

--- .bash/bin/demo-limiter.sh
+++ .bash/bin/demo-limiter.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # this is great if you have something like Discord killing your CPU
+# first install cpulimit with: sudo apt install cpulimit
 cpulimit -P /usr/share/file/path -l 35
 cpulimit --exe exacutableFile --limit 40
